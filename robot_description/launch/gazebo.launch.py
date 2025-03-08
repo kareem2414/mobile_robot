e17b965c91f20f1d7519e3bece5f353248945613
kareem2414 <kareemahmed2414@gmail.com>
2025-03-08 23:11:40 +0200

    updating the localization pkg

--- robot_description/launch/gazebo.launch.py
+++ robot_description/launch/gazebo.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution, PythonExpression
 from launch_ros.parameter_descriptions import ParameterValue
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
@@ -22,22 +22,46 @@ def generate_launch_description():
         default_value= os.path.join(get_package_share_directory("robot_description"),"urdf","robot_description.urdf.xacro")
     )
     robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]))
-    
-    robot_state_publisher = Node(
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+    world_path = PathJoinSubstitution([
+        robot_description_path,
+        "worlds",
+        PythonExpression(expression=["'",LaunchConfiguration("world_name"), "'", "+'.world'"])
+    ])
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
+    )
+
+    robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description":robot_description}]
     )
+    gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+            [os.path.join(get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+            launch_arguments={"gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])}.items())
 
-    start_gazebo_server = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(
-            get_package_share_directory("gazebo_ros"),"launch","gzserver.launch.py"
-        )))
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "bumperbot"],
+    )
 
-    start_gazebo_client = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(
-            get_package_share_directory("gazebo_ros"),"launch","gzclient.launch.py"
-        )))
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+            "/imu@sensor_msgs/msg/Imu[gz.msgs.IMU"
+        ],
+        remappings=[
+            ('/imu', '/imu/out'),
+        ]
+    )
     
     spawn_robot = Node(
         package="gazebo_ros",
@@ -46,11 +70,11 @@ def generate_launch_description():
         output= "screen"
     )
     return LaunchDescription([
-        env_variable,
         model_arg,
-        robot_state_publisher,
-        start_gazebo_server,
-        start_gazebo_client,
-        spawn_robot
-
+        world_name_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge
     ])
